69c78fa60f470609cd41c6bf339742cdd0fc6fc9
barry <barry@barryli.ca>
2024-06-05 00:35:39 -0400

    Added get additional hidden ifd exif tags.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Overlay EXIF metadata onto photos.
 ## Features
 - Dynamic overlay colour.
 - Overlay orientation, landscape vs portrait mode.
+- QR Code for more information
 
 ## Development
 ```bash
--- exiffusion/extract_exif.py
+++ exiffusion/extract_exif.py
@@ -3,29 +3,80 @@ from PIL.Image import Exif
 from PIL.ExifTags import TAGS, GPSTAGS
 from pillow_heif import register_heif_opener
 
-from typing import Tuple, Union, Dict
+from typing import Tuple
 from pydantic import BaseModel
 
 register_heif_opener()
 
 GPSINFO_IFD_KEY = 34853
+GENERAL_IFD_KEY = 34665
 
 
-# TODO: Handle missing fields
-class RelevantExifTags(BaseModel):
-    Orientation: int
+class TopLevelExifTags(BaseModel):
+    Orientation: int = 1
     DateTime: str
-    GPSLatitudeRef: str
-    GPSLatitude: Tuple[float, float, float]
-    GPSLongitudeRef: str
-    GPSLongitude: Tuple[float, float, float]
+    Make: str = None
+    Model: str = None
+    HostComputer: str = None
+    Software: str = None
 
 
-def get_exif(img: str) -> RelevantExifTags:
+class GPSExifTags(BaseModel):
+    GPSLatitudeRef: str = None
+    GPSLatitude: Tuple[float, float, float] = None
+    GPSLongitudeRef: str = None
+    GPSLongitude: Tuple[float, float, float] = None
+
+
+class GeneralIFDTags(BaseModel):
+    ShutterSpeedValue: float = None
+    ApertureValue: float = None
+    DateTimeOriginal: str = None
+    DateTimeDigitized: str = None
+    BrightnessValue: float = None
+    ExposureBiasValue: float = None
+    MeteringMode: int = None
+    ColorSpace: int = None
+    Flash: int = None
+    FocalLength: float = None
+    ExifImageWidth: int = None
+    ExifImageHeight: int = None
+    FocalLengthIn35mmFilm: int = None
+    OffsetTime: str = None
+    SubsecTimeOriginal: str = None
+    SubjectLocation: Tuple[int, int, int, int] = None
+    SubsecTimeDigitized: str = None
+    SensingMethod: int = None
+    ExposureTime: float = None
+    FNumber: float = None
+    SceneType: int = None
+    ExposureProgram: int = None
+    ISOSpeedRatings: int = None
+    ExposureMode: int = None
+    WhiteBalance: int = None
+    LensSpecification: Tuple[float, float, float, float] = None
+    LensMake: str = None
+    LensModel: str = None
+    CompositeImage: int = None
+    # MakerNote: bytes
+
+
+class RelevantExifTags(TopLevelExifTags, GPSExifTags, GeneralIFDTags):
+    pass
+
+
+def get_exif(img: str, get_detailed_tags=False) -> RelevantExifTags:
     image = Image.open(img)
     exif = image.getexif()
 
-    relevant_tags = ["Orientation", "DateTime"]
+    relevant_tags = [
+        "Orientation",
+        "DateTime",
+        "Make",
+        "Model",
+        "HostComputer",
+        "Software",
+    ]
 
     exif_tags = {}
 
@@ -36,10 +87,17 @@ def get_exif(img: str) -> RelevantExifTags:
 
     gps_tags = get_gps(exif)
 
-    return RelevantExifTags(**(exif_tags | gps_tags))
+    if get_detailed_tags:
+        general_tags = get_general_ifd(exif)
+
+        return RelevantExifTags(
+            **(exif_tags | gps_tags.model_dump() | general_tags.model_dump())
+        )
+    else:
+        return RelevantExifTags(**(exif_tags | gps_tags.model_dump()))
 
 
-def get_gps(exif: Exif) -> Dict[str, Union[str, Tuple[float, float, float]]]:
+def get_gps(exif: Exif) -> GPSExifTags:
     gps_info = exif.get_ifd(GPSINFO_IFD_KEY)
 
     relevant_tags = ["GPSLatitudeRef", "GPSLatitude", "GPSLongitudeRef", "GPSLongitude"]
@@ -51,4 +109,54 @@ def get_gps(exif: Exif) -> Dict[str, Union[str, Tuple[float, float, float]]]:
         if gps_tag in relevant_tags:
             gps_tags[gps_tag] = value
 
-    return gps_tags
+    return GPSExifTags(**gps_tags)
+
+
+def get_general_ifd(exif: Exif) -> GeneralIFDTags:
+    general_ifd = exif.get_ifd(GENERAL_IFD_KEY)
+
+    relevant_tags = [
+        "ShutterSpeedValue",
+        "ApertureValue",
+        "DateTimeOriginal",
+        "DateTimeDigitized",
+        "BrightnessValue",
+        "ExposureBiasValue",
+        "MeteringMode",
+        "ColorSpace",
+        "Flash",
+        "FocalLength",
+        "ExifImageWidth",
+        "ExifImageHeight",
+        "FocalLengthIn35mmFilm",
+        "OffsetTime",
+        "SubsecTimeOriginal",
+        "SubjectLocation",
+        "SubsecTimeDigitized",
+        "SensingMethod",
+        "ExposureTime",
+        "FNumber",
+        "SceneType",
+        "ExposureProgram",
+        "ISOSpeedRatings",
+        "ExposureMode",
+        "WhiteBalance",
+        "LensSpecification",
+        "LensMake",
+        "LensModel",
+        "CompositeImage",
+        # "MakerNote",
+    ]
+
+    ifd_tags = {}
+
+    for tag, value in general_ifd.items():
+        ifd_tag = TAGS.get(tag, tag)
+
+        if ifd_tag in relevant_tags:
+            ifd_tags[ifd_tag] = value
+
+    if "SceneType" in ifd_tags:
+        ifd_tags["SceneType"] = ord(ifd_tags["SceneType"])
+
+    return GeneralIFDTags(**ifd_tags)
